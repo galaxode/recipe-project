ee37e0f03eb6228f6b0d58728d8087c03131f3f7
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-11 21:36:40 -0400

    Create formatting and structure for listing out ingredients in shopping-list components using ingredient model.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatillos', 10),
+    new Ingredient('Hot Sauce', 1)
+   ];
 
   constructor() { }
 
