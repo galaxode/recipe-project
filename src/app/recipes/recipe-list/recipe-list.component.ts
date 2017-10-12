aae2a6917c82f536a41cb111ff31f949c5f32cc7
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-11 20:33:52 -0400

    Add Recipe model and formatting in template.  Use hardcoded data for a test recipe.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/9/8492/28755393040_c3b4c4853d_b.jpg')
+  ];
 
   constructor() { }
 
