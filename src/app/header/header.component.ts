7aec646c4b1dca4484bb43c142187bcdef37cc00
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-13 10:20:14 -0400

    Enable navbar item buttons and conditional display of recipes and shopping list views. This will be refactored later with routes.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() navClicked = new EventEmitter<string>();
 
+  // Emit an event called navClicked which will pass the nav items id
+  onClick(navItem: HTMLElement) {
+    this.navClicked.emit(navItem.id);
+  }
 }
