e3da9ca64ebca7d92df2f1c4eadef0c0ba8a1db9
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-18 18:00:59 -0400

    Add ingredient array to recipe model and populat ingredients in the recipe detail view.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,27 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/9/8492/28755393040_c3b4c4853d_b.jpg'),
-    new Recipe('Another Test Recipe', 'This is another a test', 'https://c1.staticflickr.com/9/8492/28755393040_c3b4c4853d_b.jpg'),
+    new Recipe(
+      'Hot Dogs',
+      'This is classic favorite at barbecues!',
+      'https://c1.staticflickr.com/9/8492/28755393040_c3b4c4853d_b.jpg',
+      [
+        new Ingredient('Buns', 4),
+        new Ingredient('Weiners', 4)
+      ]),
+    new Recipe('Apple Pie',
+      'Luscious and juicy pie filled with apples. It is so very delitchous',
+      'https://c1.staticflickr.com/9/8492/28755393040_c3b4c4853d_b.jpg',
+      [
+        new Ingredient('Apples', 5),
+        new Ingredient('Pie Crust', 1)
+      ])
   ];
 
   getRecipes() {
