309c83010be52bc69d2cd0a375a9297f4bf10009
Andrew Cookro <andrew.cookro@gmail.com>
2017-10-26 15:23:19 -0400

    Add main routes and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isRecipes: boolean = true; // recipe tab is default
-  isShoppingList: boolean = false;
-
-  onNavClicked(navId: string) {
-    if (navId === 'recipes-nav') {
-      this.isRecipes = true;
-      this.isShoppingList = false;
-    }
-    if (navId === 'shopping-list-nav') {
-      this.isRecipes = false;
-      this.isShoppingList = true;
-    }
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() navClicked = new EventEmitter<string>();
 
-  // Emit an event called navClicked which will pass the nav items id
-  onClick(navItem: HTMLElement) {
-    this.navClicked.emit(navItem.id);
-  }
 }
