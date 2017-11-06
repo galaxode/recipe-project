e6fc463c5dece591ddaa88911527c6cc565d3d2d
Andrew Cookro <andrew.cookro@gmail.com>
2017-11-05 21:45:49 -0500

    Add initial Recipe Edit form which populates with current recipe data if initialized from recipe edit link or blank if initialized from new recipe link.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,17 +13,44 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  ingredients: Ingredient[] = [];
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
-        console.log(this.editMode);
+        this.initForm();
       }
     );
+  }
+
+  initForm() {
+    let name = '';
+    let imagePath = '';
+    let description = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeByIndex(this.id);
+      name = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(name, Validators.required),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(description),
+      'ingredients': new FormArray([])
+    });
+  }
+
+  onAddIngredient() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.recipeForm.get('ingredients')).push(control);
   }
 
 }
