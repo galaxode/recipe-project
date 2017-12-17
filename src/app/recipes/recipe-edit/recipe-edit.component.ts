826f7c4b8a616acee04ec59b2fb342a87d330c63
Andrew Cookro <andrew.cookro@gmail.com>
2017-12-16 20:19:50 -0500

    Fixed error when running prod build with aot stating controls property does not exist on type on AbstractControl occurring from recipe-edit template file.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -111,4 +111,8 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
 
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
 }
